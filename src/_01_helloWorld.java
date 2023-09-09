a919f5a9ed878dd903a2642f10712d438b705281
Ikk11 <wom8693@gmail.com>
2023-09-09 23:40:51 +0900

    Hello World!!!

--- src/_01_helloWorld.java
+++ src/_01_helloWorld.java
@@ -2,5 +2,6 @@ public class _01_helloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World!!!");
         System.out.println("!");
+        System.out.println("!!");
     }
 }
